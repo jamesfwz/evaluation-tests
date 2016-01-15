c53cad9e285a1a8818b0fbcf8eb926438162af4e
James Futureworkz <fwzjames@gmail.com>
2016-01-15 15:24:23 +0700

    add Relationship#ensure_valid_age

--- rails-test-6/app/models/relationship.rb
+++ rails-test-6/app/models/relationship.rb
@@ -1,12 +1,25 @@
 class Relationship < ActiveRecord::Base
   extend Enumerize
 
+  MIN_AGE = {
+    "Father" => 20,
+    "Mother" => 18
+  }
+
   belongs_to :person 
   belongs_to :member, class_name: Person
 
   validates :person_id,     presence: true
   validates :member_id,     presence: true
   validates :member_type,   presence: true
+  
+  validate :ensure_valid_age
 
   enumerize :member_type, in: [:Father, :Mother], predicates: true
+
+  private 
+
+  def ensure_valid_age
+    return errors[:member_id] << "#{member_type_text} must be over #{MIN_AGE[member_type]}" if member && member.age < MIN_AGE[member_type] 
+  end
 end
--- rails-test-6/spec/models/relationship_spec.rb
+++ rails-test-6/spec/models/relationship_spec.rb
@@ -12,4 +12,26 @@ describe Relationship do
     it { is_expected.to belong_to :person }
     it { is_expected.to belong_to :member }
   end
+
+  describe '#ensure_valid_age' do 
+    let(:father_relationship) { build(:father_relationship) }
+
+    context 'valid' do
+      let!(:peter)  { create(:male, first_name: 'Peter', dob: 22.years.ago) }
+
+      it "Father must be greater than 20" do
+        father_relationship.member = peter
+        expect(father_relationship).to be_valid
+      end
+    end
+
+    context 'invalid' do
+      let!(:peter)  { create(:male, first_name: 'Peter', dob: 19.years.ago) }
+
+      it "Father must be greater than 20" do
+        father_relationship.member = peter
+        expect(father_relationship).to be_invalid
+      end
+    end
+  end
 end
